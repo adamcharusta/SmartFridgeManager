0692ab0afcf2210d60120a2cf6c7b43b6c98b5b0
Adam Charusta <charustaadam95@gmail.com>
2024-06-15 18:25:08 +0200

    create get_repositories script, create docker-compose for dev environment

---
+++ scripts/get_repositories.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+repository_dir="repositories"
+mkdir -p ../${repository_dir}
+cd ../${repository_dir}
+
+repository_names=("SmartFridgeManagerGUI" "SmartFridgeManagerAPI")
+
+create_repository_clone_url() {
+    local repository_name=$1
+    local repository_base_url="https://github.com/adamcharusta/"
+    local repository_url_suffix=".git"
+
+    echo "$repository_base_url$repository_name$repository_url_suffix"
+}
+
+instal_dependencies() {
+    local repository_name=$1
+
+    echo "Installing dependencies for $repository_name..."
+    if [ -f $repository_name/package.json ]; then
+        cd $repository_name
+        yarn install && yarn build
+        cd - > /dev/null
+    elif [ -f $repository_name/${repository_name}.sln ]; then
+        cd $repository_name
+        dotnet restore && dotnet build
+    else
+        echo "No dependencies to install for $repository_name"
+        exit 1
+    fi
+}
+
+clone_repository() {
+    local repository_name=$1
+    local repository_base_url="https://github.com/adamcharusta/"
+    local repository_url_suffix=".git"
+
+    if [ -d ./$repository_name ]; then
+        echo "Repository $repository_name already exists. Pulling changes..."
+        cd $repository_name
+        git pull
+        cd - > /dev/null
+    else
+        local url=$(create_repository_clone_url repository_name)
+
+        echo url
+        echo "Repository $repository_name does not exist. Cloning..."
+        git clone $url
+    fi
+
+    if [ -f $repository_name ]; then
+        echo "Repository $repository_name depends on other repositories. Please clone them first."
+        exit 1
+    fi
+}
+
+
+for repository_name in "${repository_names[@]}" ; do
+    clone_repository $repository_name
+    instal_dependencies $repository_name
+done
